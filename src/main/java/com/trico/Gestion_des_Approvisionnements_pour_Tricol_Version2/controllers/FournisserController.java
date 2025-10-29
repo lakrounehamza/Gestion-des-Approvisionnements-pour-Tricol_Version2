e136745a56bcfb861ef3921631390593334ea1e5
lakrounehamza <lakrounehamza10@gmail.com>
2025-10-29 14:19:20 +0100

    feat(Contoller) :  ajoute controller   fournisseur

---
+++ src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/controllers/FournisserController.java
@@ -0,0 +1,29 @@
+package com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.controllers;
+
+import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.dto.FournisseurDto;
+import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.entitys.Fournisseur;
+import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.service.interfaces.IFournisseurService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("api/v1/fournisseurs")
+public class FournisserController {
+    public final IFournisseurService fournisseurService;
+    @PostMapping
+    public ResponseEntity<FournisseurDto>  createFournisseur(@RequestBody FournisseurDto fournisseurDto){
+        System.out.println(fournisseurDto);
+        FournisseurDto t = fournisseurDto;
+      FournisseurDto f = fournisseurService.save(fournisseurDto);
+        return ResponseEntity.of(Optional.ofNullable(f));
+    }
+    @GetMapping
+    public ResponseEntity<List<FournisseurDto>>  getAllFournisseur(){
+        return ResponseEntity.ok(fournisseurService.getAll());
+    }
+}

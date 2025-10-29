09522d42e6f8ddf6b0dc6f90440a4ba384daf9de
lakrounehamza <lakrounehamza10@gmail.com>
2025-10-29 19:58:31 +0100

    feat(DTO) :   pour   register   des   fournisser

---
+++ src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/dto/FournisseuRregistreDTO.java
@@ -0,0 +1,19 @@
+package com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FournisseuRregistreDTO {
+    private String societe;
+    private String adresse;
+    private String contact;
+    private String email;
+    private String phone;
+    private String ville;
+    private String ice;
+}

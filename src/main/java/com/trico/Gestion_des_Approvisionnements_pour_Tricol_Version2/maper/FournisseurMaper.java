a13378c1f988fdeec20390ef4d53829e8627b2f4
lakrounehamza <lakrounehamza10@gmail.com>
2025-10-29 20:04:42 +0100

    fix(mapper): separe les methodes pour transferer le DTO vers l entity

--- src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/maper/FournisseurMaper.java
+++ src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/maper/FournisseurMaper.java
@@ -1,5 +1,6 @@
 package com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.maper;
 
+import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.dto.FournisseuRregistreDTO;
 import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.dto.FournisseurDto;
 import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.entitys.Fournisseur;
 import org.mapstruct.Mapper;
@@ -8,5 +9,6 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface FournisseurMaper {
     Fournisseur  toEntity(FournisseurDto dto);
+    Fournisseur  toEntity(FournisseuRregistreDTO dto);
     FournisseurDto toDto(Fournisseur entity);
 }

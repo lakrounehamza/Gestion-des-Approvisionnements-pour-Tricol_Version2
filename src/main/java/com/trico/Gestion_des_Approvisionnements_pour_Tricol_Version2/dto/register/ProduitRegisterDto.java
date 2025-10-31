44ada4cf864740a8305b865ad884b2557da28913
lakrounehamza <lakrounehamza10@gmail.com>
2025-10-31 16:50:29 +0100

    feat(DTO) : ajoute validation pour insertion des produit

--- src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/dto/register/ProduitRegisterDto.java
+++ src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/dto/register/ProduitRegisterDto.java
@@ -3,6 +3,11 @@ package com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.dto.regist
 import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.entitys.CommandeItem;
 import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.entitys.MouvementsStock;
 import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.enums.CategorieEnum;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,10 +20,25 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class ProduitRegisterDto {
+
+    @NotBlank(message = "Le nom du produit est obligatoire.")
+    @Size(min = 2, max = 100, message = "Le nom du produit doit contenir entre 2 et 100 caractères.")
     private String nom;
+
+    @NotNull(message = "Le prix du produit est obligatoire.")
+    @DecimalMin(value = "0.1", message = "Le prix doit être supérieur à 0.")
     private double prix;
+
+    @NotBlank(message = "La description du produit est obligatoire.")
+    @Size(max = 255, message = "La description ne doit pas dépasser 255 caractères.")
     private String description;
+
+    @NotNull(message = "La catégorie du produit est obligatoire.")
     private CategorieEnum categorie;
-    private List<CommandeItem> CommandeItem;
+
+    @Valid
+    private List<CommandeItem> commandeItems;
+
+    @Valid
     private List<MouvementsStock> mouvementsStock;
 }

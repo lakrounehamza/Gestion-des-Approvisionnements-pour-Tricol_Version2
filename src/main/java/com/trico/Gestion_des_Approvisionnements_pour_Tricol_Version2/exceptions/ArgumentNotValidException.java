591d08c64b6cada65d5fd42f5b265644b5842a4a
lakrounehamza <lakrounehamza10@gmail.com>
2025-10-31 16:11:24 +0100

    feat(Exception) :  ajouter les exception

---
+++ src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/exceptions/ArgumentNotValidException.java
@@ -0,0 +1,7 @@
+package com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.exceptions;
+
+public class ArgumentNotValidException extends RuntimeException {
+    public ArgumentNotValidException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}

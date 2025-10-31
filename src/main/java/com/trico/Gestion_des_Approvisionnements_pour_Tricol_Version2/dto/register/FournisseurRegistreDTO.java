ae901fac04de77e30f10f2e059e70bb96816b7e2
lakrounehamza <lakrounehamza10@gmail.com>
2025-10-31 16:48:20 +0100

    feat(DTO) : ajoute validation pour insertion des fournisseur

--- src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/dto/register/FournisseurRegistreDTO.java
+++ src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/dto/register/FournisseurRegistreDTO.java
@@ -1,4 +1,9 @@
 package com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.dto.register;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,12 +13,34 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class FournisseuRregistreDTO {
+public class FournisseurRegistreDTO {
+
+    @NotBlank(message = "Le nom de la société est obligatoire.")
+    @Size(min = 2, max = 100, message = "Le nom de la société doit contenir entre 2 et 100 caractères.")
     private String societe;
+
+    @NotBlank(message = "L’adresse est obligatoire.")
+    @Size(max = 255, message = "L’adresse ne doit pas dépasser 255 caractères.")
     private String adresse;
+
+    @NotBlank(message = "Le nom du contact est obligatoire.")
+    @Size(min = 2, max = 100, message = "Le nom du contact doit contenir entre 2 et 100 caractères.")
     private String contact;
+
+    @NotBlank(message = "L’adresse email est obligatoire.")
+    @Email(message = "L’adresse email doit être valide.")
     private String email;
+
+    @NotBlank(message = "Le numéro de téléphone est obligatoire.")
+    @Pattern( regexp = "^(\\+\\d{1,3})?[- .]?\\d{2,4}([- .]?\\d{2,4}){2,3}$",
+             message = "Le numéro de téléphone doit être valide (ex : +212XXXXXXXXX).")
     private String phone;
+
+    @NotBlank(message = "La ville est obligatoire.")
+    @Size(min = 2, max = 50, message = "Le nom de la ville doit contenir entre 2 et 50 caractères.")
     private String ville;
+
+    @NotBlank(message = "Le code ICE est obligatoire.")
+    @Pattern(regexp = "^[0-9]{8,15}$", message = "Le code ICE doit contenir uniquement des chiffres (entre 8 et 15 caractères).")
     private String ice;
 }

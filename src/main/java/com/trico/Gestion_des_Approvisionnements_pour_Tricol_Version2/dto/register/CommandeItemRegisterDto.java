928479d4799e3116f8abe0be5cfdcf38d0687376
lakrounehamza <lakrounehamza10@gmail.com>
2025-10-31 16:43:59 +0100

    feat(DTO) : ajoute  le validation

--- src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/dto/register/CommandeItemRegisterDto.java
+++ src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/dto/register/CommandeItemRegisterDto.java
@@ -2,6 +2,9 @@ package com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.dto.regist
 
 import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.entitys.Commande;
 import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.entitys.Produit;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,8 +17,17 @@ import java.util.Date;
 @NoArgsConstructor
 @Builder
 public class CommandeItemRegisterDto {
+
+    @Min(value = 1, message = "La quantité doit être au minimum de 1.")
     private int quantity;
+
+    @NotNull(message = "La date ne peut pas être nulle.")
+    @PastOrPresent(message = "La date ne peut pas être dans le futur.")
     private Date date;
+
+    @NotNull(message = "La commande ne peut pas être nulle.")
     private Commande commande;
+
+    @NotNull(message = "Le produit ne peut pas être nul.")
     private Produit produit;
 }

703a2f080fb86f302a4543d101a0138a4cae59b7
lakrounehamza <lakrounehamza10@gmail.com>
2025-10-31 16:46:43 +0100

    feat(DTO) :  ajoute  le validation pour insertion des commande

--- src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/dto/register/CommandeRegisterDto.java
+++ src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/dto/register/CommandeRegisterDto.java
@@ -3,6 +3,8 @@ package com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.dto.regist
 import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.entitys.CommandeItem;
 import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.entitys.Fournisseur;
 import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.enums.StatutCommandeEnum;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,10 +18,26 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class CommandeRegisterDto {
+
+    @NotNull(message = "Le statut de la commande est obligatoire.")
     private StatutCommandeEnum statut;
+
+    @Positive
+    @NotNull(message = "Le montant ne peut pas être nul.")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Le montant doit être supérieur à 0.")
     private Double montant;
+
+    @NotNull(message = "La date de commande est obligatoire.")
+    @PastOrPresent(message = "La date de commande ne peut pas être dans le futur.")
     private Date dateCommande;
-    private List<CommandeItem> CommandeItem;
+
+    @NotEmpty(message = "La commande doit contenir au moins un article.")
+    @Valid
+    private List<CommandeItem> commandeItems;
+
+    @NotNull(message = "Le fournisseur est obligatoire.")
     private Fournisseur fournisseur;
-    private Fournisseur clinet;
+
+    @NotNull(message = "Le client est obligatoire.")
+    private Fournisseur client;
 }

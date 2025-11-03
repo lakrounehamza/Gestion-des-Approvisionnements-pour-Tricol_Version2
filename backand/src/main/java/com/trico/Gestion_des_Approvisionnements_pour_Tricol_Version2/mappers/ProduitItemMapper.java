cfda7471c0d8b613970c5fd36b84d34a0cee55c6
lakrounehamza <lakrounehamza10@gmail.com>
2025-11-03 16:44:14 +0100

    feat(mapper)  : ajoute  mapper produit  item

---
+++ backand/src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/mappers/ProduitItemMapper.java
@@ -0,0 +1,11 @@
+package com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.mappers;
+
+import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.dto.ProduitItemDto;
+import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.dto.register.ProduitItemRegisterDto;
+import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.entitys.ProduitItem;
+
+public interface ProduitItemMapper {
+    ProduitItem toEntity(ProduitItemDto produitItemDto);
+    ProduitItemDto toDto(ProduitItem produitItem);
+    ProduitItem toEntity(ProduitItemRegisterDto produitItemRegisterDto);
+}

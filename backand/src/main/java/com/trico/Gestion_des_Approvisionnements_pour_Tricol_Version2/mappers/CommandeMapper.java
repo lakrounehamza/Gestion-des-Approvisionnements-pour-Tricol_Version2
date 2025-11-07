007f716bd839edc13537b8cdd50b3b641ed8ddf1
lakrounehamza <lakrounehamza10@gmail.com>
2025-11-07 20:58:35 +0100

    refactor(mapper) :  refactor mapper de commande

--- backand/src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/mappers/CommandeMapper.java
+++ backand/src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/mappers/CommandeMapper.java
@@ -4,10 +4,13 @@ import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.dto.Command
 import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.dto.register.CommandeRegisterDto;
 import com.trico.Gestion_des_Approvisionnements_pour_Tricol_Version2.entitys.Commande;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CommandeMapper {
     Commande toEntity(CommandeDto dto);
     Commande toEntity(CommandeRegisterDto dto);
+    @Mapping(source = "client", target = "client")
+    @Mapping(source = "fournisseur", target = "fournisseur")
     CommandeDto toDto(Commande entity);
 }
--- backand/src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/mappers/ProduitMapper.java
+++ backand/src/main/java/com/trico/Gestion_des_Approvisionnements_pour_Tricol_Version2/mappers/ProduitMapper.java
@@ -8,6 +8,8 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface ProduitMapper {
     Produit toEntity(ProduitRegisterDto produitRegisterDto);
-    Produit  toEntity(ProduitDto produitDto);
+
+    Produit toEntity(ProduitDto produitDto);
+
     ProduitDto toDto(Produit produit);
 }
